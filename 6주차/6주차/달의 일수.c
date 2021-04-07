168e8dbf7051401869146f1a9f032cf055ae5ce6
이요한 <john11113@naver.com>
2021-04-07 17:09:36 +0900

    04.07 수업

--- 6주차/6주차/달의 일수.c
+++ 6주차/6주차/달의 일수.c
@@ -1,1 +1,34 @@
-#
+#include<stdio.h>
+
+int main(void) {
+
+	int month, days;
+
+	printf("달을 입력하세요 :");
+	scanf_s("%d",&month);
+
+	switch (month)
+	{
+
+	case 2:
+		days = 28;
+		break;
+	case 4:
+	case 6:	 
+	case 9:	 
+	case 11:
+		days = 30;
+		break; 
+	default:
+		days = 31;
+		break;
+
+	}
+
+	printf("%d월의 일수는 %d일 입니다.\n", month, days);
+
+
+
+
+	return 0;
+}
